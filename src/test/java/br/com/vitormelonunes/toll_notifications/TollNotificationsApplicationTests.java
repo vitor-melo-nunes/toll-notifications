5930bd6b4c992dd6e2bc0d4a58c0852ee3c300c2
Vitor de Melo Nunes <vitor.nunes@gruposantander.com>
2024-06-21 18:05:04 -0300

    Initial commit

---
+++ src/test/java/br/com/vitormelonunes/toll_notifications/TollNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitormelonunes.toll_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
